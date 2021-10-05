f569fe3c2d62293ec72a8f7b9943fe966a3f591f
Comma Device <device@comma.ai>
2021-10-05 09:03:40 +0200

    HACK: only set safety mode on first panda

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -51,10 +51,11 @@ std::string get_time_str(const struct tm &time) {
 void safety_setter_thread(std::vector<Panda *> pandas) {
   LOGD("Starting safety setter thread");
 
-  for (const auto& panda : pandas) {
-    // diagnostic only is the default, needed for VIN query
-    panda->set_safety_model(cereal::CarParams::SafetyModel::ELM327);
-  }
+  // for (const auto& panda : pandas) {
+  //   // diagnostic only is the default, needed for VIN query
+  //   panda->set_safety_model(cereal::CarParams::SafetyModel::ELM327);
+  // }
+  pandas[0]->set_safety_model(cereal::CarParams::SafetyModel::ELM327);
 
   Params p = Params();
 
@@ -77,10 +78,11 @@ void safety_setter_thread(std::vector<Panda *> pandas) {
     util::sleep_for(20);
   }
 
-  for (const auto& panda : pandas) {
-    // VIN query done, stop listening to OBDII
-    panda->set_safety_model(cereal::CarParams::SafetyModel::ELM327, 1);
-  }
+  // for (const auto& panda : pandas) {
+  //   // VIN query done, stop listening to OBDII
+  //   panda->set_safety_model(cereal::CarParams::SafetyModel::ELM327, 1);
+  // }
+  pandas[0]->set_safety_model(cereal::CarParams::SafetyModel::ELM327, 1);
 
   std::string params;
   LOGW("waiting for params to set safety model");
@@ -105,17 +107,19 @@ void safety_setter_thread(std::vector<Panda *> pandas) {
   cereal::CarParams::Reader car_params = cmsg.getRoot<cereal::CarParams>();
   cereal::CarParams::SafetyModel safety_model = car_params.getSafetyModel();
 
-  for (const auto& panda : pandas) {
-    panda->set_unsafe_mode(0);  // see safety_declarations.h for allowed values
-  }
+  // for (const auto& panda : pandas) {
+  //   panda->set_unsafe_mode(0);  // see safety_declarations.h for allowed values
+  // }
+  pandas[0]->set_unsafe_mode(0);  // see safety_declarations.h for allowed values
 
   auto safety_param = car_params.getSafetyParam();
   LOGW("setting safety model: %d with param %d", (int)safety_model, safety_param);
 
-  for (const auto& panda : pandas) {
-    // TODO: do we need to add a safety param which is panda index specific?
-    panda->set_safety_model(safety_model, safety_param);
-  }
+  // for (const auto& panda : pandas) {
+  //   // TODO: do we need to add a safety param which is panda index specific?
+  //   panda->set_safety_model(safety_model, safety_param);
+  // }
+  pandas[0]->set_safety_model(safety_model, safety_param);
 
   safety_setter_thread_running = false;
 }
